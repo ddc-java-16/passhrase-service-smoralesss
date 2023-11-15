df4ea806d68a4d206a4362dee1f2d5ce58149380
sam morales <smorales40@cnm.edu>
2023-11-15 16:59:28 -0700

    Added UUID deserializer

---
+++ src/main/java/edu/cnm/deepdive/passphrase/view/UUIDDeserializer.java
@@ -0,0 +1,27 @@
+package edu.cnm.deepdive.passphrase.view;
+
+import static jdk.internal.org.jline.utils.Colors.s;
+
+import com.fasterxml.jackson.databind.util.StdConverter;
+import edu.cnm.deepdive.passphrase.service.UUIDStringifier;
+import java.util.UUID;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UUIDDeserializer extends StdConverter<String, UUID> implements Converter<String, UUID> {
+
+  private final UUIDStringifier stringifier;
+
+  public UUIDDeserializer(UUIDStringifier stringifier) {
+    this.stringifier = stringifier;
+  }
+
+
+
+
+  @Override
+  public UUID convert(String s) {
+    return stringifier.toUUID(s);
+  }
+}
